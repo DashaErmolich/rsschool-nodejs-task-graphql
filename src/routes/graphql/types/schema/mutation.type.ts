415935859ce5a0c2f1da4db071d06aec56d5184b
DashaErmolich <dasha.ermolich@gmail.com>
2024-03-01 18:26:31 +0100

    feat: mutations for delete resources

--- src/routes/graphql/types/schema/mutation.type.ts
+++ src/routes/graphql/types/schema/mutation.type.ts
@@ -1,11 +1,12 @@
-import { GraphQLObjectType } from 'graphql';
+import { GraphQLBoolean, GraphQLObjectType } from 'graphql';
 import { CreatePostInputType, PostType } from '../post.type.js';
 import { Context } from '../../models/context.model.js';
 import { CreateUserInputType, UserType } from '../user.type.js';
 import { CreateProfileInputType, ProfileType } from '../profile.type.js';
-import { CreateProfileInputDto } from '../../models/profile.model.js';
-import { CreateUserInputDto } from '../../models/user.model.js';
-import { CreatePostInputDto } from '../../models/post.model.js';
+import { CreateProfileInputDto, Profile } from '../../models/profile.model.js';
+import { CreateUserInputDto, User } from '../../models/user.model.js';
+import { CreatePostInputDto, Post } from '../../models/post.model.js';
+import { UUIDType } from '../uuid.type.js';
 
 interface MutationData<T> {
   dto: T;
@@ -24,6 +25,18 @@ export const MutationType: GraphQLObjectType<
         return await dataClient.post.create({ data: dto });
       },
     },
+    deletePost: {
+      type: GraphQLBoolean,
+      args: { id: { type: UUIDType } },
+      resolve: async (_source, { id: postId }: Post, { dataClient }, _info) => {
+        try {
+          await dataClient.post.delete({ where: { id: postId } });
+          return true;
+        } catch {
+          return false;
+        } 
+      },
+    },
     createUser: {
       type: UserType,
       args: { dto: { type: CreateUserInputType } },
@@ -31,11 +44,35 @@ export const MutationType: GraphQLObjectType<
         return await dataClient.user.create({ data: dto });
       },
     },
+    deleteUser: {
+      type: GraphQLBoolean,
+      args: { id: { type: UUIDType } },
+      resolve: async (_source, { id: userId }: User, { dataClient }, _info) => {
+        try {
+          await dataClient.user.delete({ where: { id: userId } });
+          return true;
+        } catch {
+          return false;
+        }
+      },
+    },
     createProfile: {
       type: ProfileType,
       args: { dto: { type: CreateProfileInputType } },
       resolve: async (_source, { dto }: MutationData<CreateProfileInputDto>, { dataClient }, _info) => {
         return await dataClient.profile.create({ data: dto });
+      },
+    },
+    deleteProfile: {
+      type: GraphQLBoolean,
+      args: { id: { type: UUIDType } },
+      resolve: async (_source, { id: profileId }: Profile, { dataClient }, _info) => {
+        try {
+          await dataClient.profile.delete({ where: { id: profileId } });
+          return true;
+        } catch {
+          return false;
+        }
       },
     },
   }),
